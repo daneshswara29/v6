20703e67dbf818b9555c470cdea44cc84af65a42
daneshswara29 <44220709+daneshswara29@users.noreply.github.com>
2024-04-22 16:36:09 +0700

    Add files via upload

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -1,57 +1,116 @@
 #!/bin/bash
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-# Link Hosting Kalian
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
 
-apt install rclone -y
-printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/daneshswara29/IndoSc/main/backup/rclone.conf"
-#Install Wondershaper
-cd /bin
-git clone  https://github.com/magnific0/wondershaper.git
-cd wondershaper
-sudo make install
-cd
-rm -rf wondershaper
-echo > /home/limit
-apt install msmtp-mta ca-certificates bsd-mailx -y
-cat<<EOF>>/etc/msmtprc
-defaults
-tls on
-tls_starttls on
-tls_trust_file /etc/ssl/certs/ca-certificates.crt
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
 
-account default
-host smtp.gmail.com
-port 587
-auth on
-user okysmilee2@gmail.com
-from okysmilee2@gmail.com
-password danestkj 
-logfile ~/.msmtp.log
-EOF
-chown -R www-data:www-data /etc/msmtprc
-cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/6/IndoSc/main/backup/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/daneshswara29/IndoSc/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/daneshswara29/IndoSc/main/backup/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/daneshswara29/IndoSc/main/backup/strt.sh"
-wget -O limitspeed "https://raw.githubusercontent.com/daneshswara29/IndoSc/main/backup/limitspeed.sh"
-chmod +x autobackup
-chmod +x backup
-chmod +x restore
-chmod +x strt
-chmod +x limitspeed
-rm -f /root/set-br.sh
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/daneshswara29/izin/main/ip > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/daneshswara29/izin/main/ip | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/daneshswara29/izin/main/ip | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+PERMISSION
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+#fi
+#sleep 3
+#echo -e "
+#"
+date
 echo ""
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Checking... "
+sleep 2
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Download & Install rclone... "
+curl -fsSL https://rclone.org/install.sh | bash > /dev/null 2>&1
+printf "q\n" | rclone config > /dev/null 2>&1
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Downloading rclone config ... "
+wget -q -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/daneshswara29/v6/main/backup/rclone.conf"
+git clone https://github.com/magnific0/wondershaper.git &> /dev/null
+cd wondershaper
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Installing wondershaper... "
+make install > /dev/null 2>&1
+cd
+rm -rf wondershaper > /dev/null 2>&1
+echo > /home/limit
+
+pkgs='msmtp-mta ca-certificates bsd-mailx'
+if ! dpkg -s $pkgs > /dev/null 2>&1; then
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Installing... "
+apt install -y $pkgs > /dev/null 2>&1
+else
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Already Installed... "
+fi
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Creating service... "
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Downloading files... "
+wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/daneshswara29/v6/main/backup/backup.sh" && chmod +x /usr/bin/backup
+wget -q -O /usr/bin/restore "https://raw.githubusercontent.com/daneshswara29/v6/main/backup/restore.sh" && chmod +x /usr/bin/restore
+
+service cron restart > /dev/null 2>&1
+
+rm -f /root/set-br.sh
